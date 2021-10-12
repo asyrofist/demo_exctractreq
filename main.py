057aa707310f2eacdf925276e1dfabb5da30839d
Rakha Asyrofi <rakhasyrofist@gmail.com>
2021-10-12 15:38:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from apps import ekscase, ekspart, visualize # import your app modules here
 
 app = MultiApp()
 app.add_app("ekspart_module", ekspart.app) # Add all your application here
-app.add_app("ekscase_modeul", ekscase.app)
-app.add_app("visualize", visualize.app)
+app.add_app("ekscase_module", ekscase.app)
+app.add_app("visualize_module", visualize.app)
 app.run() # The main app
